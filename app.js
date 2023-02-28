1ad94974ac55ea3e82073589e2686023663e7559
hiramramirez <hiram.j.ramirez@gmail.com>
2023-02-27 21:44:12 -0600

    quitando letras que no conoce

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let contador = 0
 const audio = new Audio("https://cdn.freesound.org/previews/221/221683_1015240-lq.mp3");
 
 let vocales = ['a', 'e', 'i', 'o', 'u'];
-let consonantes = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'ñ', 'p', 'r', 's', 't', 'v', 'y', 'z'];
+let consonantes = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't'];
 
 boton.addEventListener('click', () => {
   let consonante1 = consonantes[consonanteRandom()];
