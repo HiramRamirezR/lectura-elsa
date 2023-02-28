037f921641acf3af84de11abadd408d1a5778a4e
hiramramirez <hiram.j.ramirez@gmail.com>
2023-02-27 18:12:10 -0600

    las palabras se forman, se agregó un contador y tiene sonido de magia

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const boton = document.querySelector('#boton');
+const boton2 = document.querySelector('#boton2');
 const texto = document.querySelector('#texto');
+const cuenta = document.querySelector('#cuenta');
+let contador = 0
+
+const audio = new Audio("https://cdn.freesound.org/previews/221/221683_1015240-lq.mp3");
 
 let vocales = ['a', 'e', 'i', 'o', 'u'];
 let consonantes = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'ñ', 'p', 'r', 's', 't', 'v', 'y', 'z'];
@@ -10,8 +15,15 @@ boton.addEventListener('click', () => {
   let consonante2 = consonantes[consonanteRandom()];
   let vocal2 = vocales[vocalRandom()];
 
-  texto.innerHTML = `${consonante1}${vocal1}${consonante2}${vocal2}`;
+  audio.play();
+
+  texto.innerHTML = `${consonante1.toUpperCase()}${vocal1}${consonante2}${vocal2}`;
 })
+
+boton2.addEventListener('click', () => {
+  contador++
+  cuenta.innerHTML = contador
+});
 
 vocalRandom = () => {
   return Math.floor(Math.random() * vocales.length);
