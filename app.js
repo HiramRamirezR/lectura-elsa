5f42c9051683ae21ab4ef023dcab2294c150a2ee
hiramramirez <hiram.j.ramirez@gmail.com>
2023-03-29 11:21:37 -0600

    Se agregó un array con 100 palabras para leer.

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
 const img = document.querySelector("img");
 
 const boton = document.querySelector('#boton');
-const boton2 = document.querySelector('#boton2');
 const texto = document.querySelector('#texto');
 const cuenta = document.querySelector('#cuenta');
 let contador = 0
 
 const audio = new Audio("https://cdn.freesound.org/previews/221/221683_1015240-lq.mp3");
 
-let vocales = ['a', 'e', 'i', 'o', 'u'];
-let consonantes = ['b', 'c', 'd', 'f', 'g', 'j', 'l', 'm', 'n', 'p', 'r', 's', 't'];
+const palabras = ["mamá", "papá", "gato", "pero", "casa", "mesa", "pelota", "tigre", "león", "ratón", "color", "nariz", "oso", "cabeza", "cochino", "pecesito", "sol", "luna", "mano", "pie", "elefante", "jirafa", "camión", "bicicleta", "fresa", "plátano", "naranja", "uva", "manzana", "sandía", "papaya", "melón", "cereza", "ciruela", "mango", "kiwi", "durazno", "piña", "limón", "lima", "zanahoria", "tomate", "cebolla", "papa", "maíz", "brócoli", "coliflor", "pepino", "chile", "ajo", "queso", "leche", "huevo", "pollo", "carne", "pescado", "arroz", "frijol", "pan", "galleta", "pastel", "helado", "chocolate", "azúcar", "sal", "aceite", "agua", "refresco", "jugo", "café", "té", "mes", "sol", "luna", "mar", "río", "montaña", "valle", "ciudad", "país", "casa", "escuela", "parque", "juego", "trabajo", "amigo", "familia", "fiesta", "viaje", "música", "arte", "película", "libro", "foto", "televisión", "ordenador"];
+
+// let vocales = ['a', 'e', 'i', 'o', 'u'];
+// let consonantes = ['b', 'c', 'd', 'f', 'g', 'j', 'l', 'm', 'n', 'p', 'r', 's', 't'];
 
 boton.addEventListener('click', () => {
-  let consonante1 = consonantes[consonanteRandom()];
-  let vocal1 = vocales[vocalRandom()];
-  let consonante2 = consonantes[consonanteRandom()];
-  let vocal2 = vocales[vocalRandom()];
+  // let consonante1 = consonantes[consonanteRandom()];
+  // let vocal1 = vocales[vocalRandom()];
+  // let consonante2 = consonantes[consonanteRandom()];
+  // let vocal2 = vocales[vocalRandom()];
+
+  contador++;
+  cuenta.innerHTML = contador;
+
+  texto.innerHTML = palabras[Math.floor(Math.random() * palabras.length)];
 
   audio.play();
   img.src = imagesArray[Math.floor(Math.random() * imagesArray.length)];
 
-  texto.innerHTML = `${consonante1}${vocal1}${consonante2}${vocal2}`;
+  // texto.innerHTML = `${consonante1}${vocal1}${consonante2}${vocal2}`;
 })
 
-boton2.addEventListener('click', () => {
-  contador++
-  cuenta.innerHTML = contador
-});
+// vocalRandom = () => {
+//   return Math.floor(Math.random() * vocales.length);
+// }
 
-vocalRandom = () => {
-  return Math.floor(Math.random() * vocales.length);
-}
-
-consonanteRandom = () => {
-  return Math.floor(Math.random() * consonantes.length);
-}
+// consonanteRandom = () => {
+//   return Math.floor(Math.random() * consonantes.length);
+// }
 
 const imagesArray = [
   "https://i.pinimg.com/originals/49/23/3a/49233a809e73d3be445a79c58e523f7f.gif",
