da678874c1511a0cffe17cbacafb9af19b2807ba
hiramramirez <hiram.j.ramirez@gmail.com>
2023-03-29 11:37:24 -0600

    Cambio de estilos

--- app.js
+++ app.js
@@ -7,16 +7,10 @@ let contador = 0
 
 const audio = new Audio("https://cdn.freesound.org/previews/221/221683_1015240-lq.mp3");
 
-const palabras = ["mamá", "papá", "gato", "pero", "casa", "mesa", "pelota", "tigre", "león", "ratón", "color", "nariz", "oso", "cabeza", "cochino", "pecesito", "sol", "luna", "mano", "pie", "elefante", "jirafa", "camión", "bicicleta", "fresa", "plátano", "naranja", "uva", "manzana", "sandía", "papaya", "melón", "cereza", "ciruela", "mango", "kiwi", "durazno", "piña", "limón", "lima", "zanahoria", "tomate", "cebolla", "papa", "maíz", "brócoli", "coliflor", "pepino", "chile", "ajo", "queso", "leche", "huevo", "pollo", "carne", "pescado", "arroz", "frijol", "pan", "galleta", "pastel", "helado", "chocolate", "azúcar", "sal", "aceite", "agua", "refresco", "jugo", "café", "té", "mes", "sol", "luna", "mar", "río", "montaña", "valle", "ciudad", "país", "casa", "escuela", "parque", "juego", "trabajo", "amigo", "familia", "fiesta", "viaje", "música", "arte", "película", "libro", "foto", "televisión", "ordenador"];
+const palabras = ["mamá", "papá", "lobo", "gato", "pero", "casa", "mesa", "pelota", "tigre", "león", "ratón", "color", "nariz", "oso", "cabeza", "cochino", "pecesito", "sol", "luna", "mano", "pie", "elefante", "jirafa", "camión", "bicicleta", "fresa", "plátano", "naranja", "uva", "manzana", "sandía", "papaya", "melón", "cereza", "ciruela", "mango", "kiwi", "durazno", "piña", "limón", "lima", "zanahoria", "tomate", "cebolla", "papa", "maíz", "brócoli", "coliflor", "pepino", "chile", "ajo", "queso", "leche", "huevo", "pollo", "carne", "pescado", "arroz", "frijol", "pan", "galleta", "pastel", "helado", "chocolate", "azúcar", "sal", "aceite", "agua", "refresco", "jugo", "café", "té", "mes", "sol", "luna", "mar", "río", "montaña", "valle", "ciudad", "país", "casa", "escuela", "parque", "juego", "trabajo", "amigo", "familia", "fiesta", "viaje", "música", "arte", "película", "libro", "foto", "televisión", "ordenador"];
 
-// let vocales = ['a', 'e', 'i', 'o', 'u'];
-// let consonantes = ['b', 'c', 'd', 'f', 'g', 'j', 'l', 'm', 'n', 'p', 'r', 's', 't'];
 
 boton.addEventListener('click', () => {
-  // let consonante1 = consonantes[consonanteRandom()];
-  // let vocal1 = vocales[vocalRandom()];
-  // let consonante2 = consonantes[consonanteRandom()];
-  // let vocal2 = vocales[vocalRandom()];
 
   contador++;
   cuenta.innerHTML = contador;
@@ -25,17 +19,7 @@ boton.addEventListener('click', () => {
 
   audio.play();
   img.src = imagesArray[Math.floor(Math.random() * imagesArray.length)];
-
-  // texto.innerHTML = `${consonante1}${vocal1}${consonante2}${vocal2}`;
 })
-
-// vocalRandom = () => {
-//   return Math.floor(Math.random() * vocales.length);
-// }
-
-// consonanteRandom = () => {
-//   return Math.floor(Math.random() * consonantes.length);
-// }
 
 const imagesArray = [
   "https://i.pinimg.com/originals/49/23/3a/49233a809e73d3be445a79c58e523f7f.gif",
@@ -52,5 +36,15 @@ const imagesArray = [
   "https://38.media.tumblr.com/6e6f98d471cafd7e1fe6f80d1d173c4e/tumblr_my8tk1XmVg1qdny4ho3_250.gif",
   "https://media.tenor.com/OT8woqzsF9cAAAAM/frozen2-disney.gif",
   "https://media.tenor.com/eYlL3-9G_rMAAAAC/frozen2-olaf.gif",
-  "https://www.wdw-magazine.com/wp-content/uploads/2020/08/olaf-hugging-snowgies.gif"
+  "https://www.wdw-magazine.com/wp-content/uploads/2020/08/olaf-hugging-snowgies.gif",
+  "https://i.pinimg.com/originals/c4/7e/f4/c47ef4b91aff08466a5cd3e29bcc87f7.gif",
+  "https://media2.giphy.com/media/BFg8adASnmMrS/giphy.gif",
+  "https://media.tenor.com/q9M0_o9PJbAAAAAd/elsa-frozen2.gif",
+  "https://www.cartonionline.com/wordpress/wp-content/uploads/2023/03/elsa_ai_11.jpg",
+  "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/hc_480x270/public/media/image/2013/11/264787-critica-frozen-reino-hielo.jpg?itok=H7m7316x",
+  "https://estaticos-cdn.elperiodico.com/clip/98ea9ae9-87b2-420e-8667-d592b57d5462_alta-libre-aspect-ratio_default_0.jpg",
+  "https://tendencybook.com/wp-content/uploads/2017/05/Frozen-portada.jpg",
+  "https://i.gifer.com/origin/cd/cd24199d048fc84e7681b7c20aa87b89.gif",
+  "https://img.buzzfeed.com/buzzfeed-static/static/2023-02/9/21/asset/83fa98a6d09e/anigif_sub-buzz-1726-1675978357-1.gif",
+  "https://sm.mashable.com/mashable_it/photo/default/frozen2-characters-hugging-700x315_mu6e.jpg"
 ]
